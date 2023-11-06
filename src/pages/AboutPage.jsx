efe7c5d9f7d670cfe65cc56dc1d1fd583152ad04
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-06 02:41:07 -0600

    updated styling to the about Me page. Larger font, moved text paragraphs into the empty space next to profile picture

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,29 +8,37 @@ export default function AboutPage() {
 
   return (
     <div className="container pt-4">
-     <img 
-      src="src/assets/img/Profile.jpeg" alt="logo"
-      height="500px"
-      width="400px"
-      className="a"
-       />
-    <p>
+    <div className="row">
+      <div className="col-md-6">
+        <img 
+          src="src/assets/img/Profile.jpeg" alt="logo"
+          height="500px"
+          width="400px"
+          className="img-fluid"
+             />
+          </div>
+          <div className="col-md-6">
+
+    <p className="lead">
             My name is Donnie Rawlings, and I decided to change my career after twenty-two years of working in retail and restaurant upper management. 
         I enjoy problem solving and finding unique ways to overcome daily hurdles as they arise needing quick solutions. 
     </p>
     
-    <p>    
+    <p className="lead">    
         In the last five years I have been involved in the roll out of multiple new divisions of my company. Most recently as part of enhancing the performace of my division I have been involved
         with teams to develop applications and websites to improve efficiency. I have really thrived in that space. This made me excited to make a leap into a new field. I am certain that joining your
         team would be mutually benificial. 
     </p>
     
-    <p>
+    <p className="lead">
         I bring with me not only decades of problem solving, but also the experience of managing teams through constantly chaning work environments. I have my MBA in business managment,
         and am currently working through a full stack bootcamp at UC Davis.
     
     </p>
         </div>
-      );
+        </div>
+        </div>
+      )
     }
+    
     
