964c508af4342f81fb3199f3af1ee7d7ce30ba5b
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-07 00:10:42 -0600

    update to image pathing for react images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import gitHub from '../assets/img/Github.png';
+import linkedIn from '../assets/img/linkedin.png';
+import devTo from '../assets/img/Devto.png';
 
 export function Footer() {
  return (
@@ -8,17 +10,17 @@ export function Footer() {
              <div className="row">
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href={gitHub} target="_blank" rel="noopener noreferrer"><img src ="src/assets/img/Github.png" height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href={"https://github.com/drawlin22"} target="_blank" rel="noopener noreferrer"><img src ={gitHub} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img src="src/assets/img/Devto.png"height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img src={linkedIn} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img src="src/assets/img/linkedin.png"height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img src={devTo} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
              </div>
