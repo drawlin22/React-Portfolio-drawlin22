bf3ba102cc8d717a4ecaee90515a8bb800fe7905
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-06 23:53:13 -0600

    removed duplicate react files

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
     <div className="row">
       <div className="col-md-6">
         <img 
-          src="dist/assets/index-d0f68184/Profile.jpeg" alt="Donnie Rawlings"
+          src="src/assets/img/Profile.jpeg" alt="Donnie Rawlings"
           height="100px"
           width="400px"
           className="img-fluid"
