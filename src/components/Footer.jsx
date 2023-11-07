b7348bfb1809822464829a24de3fb182d226ff87
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-07 00:45:13 -0600

    fixed misfiring links in the footers from linked in/dev to

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,12 +15,12 @@ export function Footer() {
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img src={linkedIn} height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img src={devTo} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img src={devTo} height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img src={linkedIn}height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
              </div>
