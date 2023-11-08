cd07dc536649d5314f437f441d974249814a7f27
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-07 20:42:23 -0600

    added spin on hoover to footer images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,17 @@ export function Footer() {
              <div className="row">
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href={"https://github.com/drawlin22"} target="_blank" rel="noopener noreferrer"><img src ={gitHub} height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href={"https://github.com/drawlin22"} target="_blank" rel="noopener noreferrer"><img className="spin-on-hover" src ={gitHub} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img src={devTo} height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://dev.to/drawlin22" target="_blank" rel="noopener noreferrer"><img className="spin-on-hover" src={devTo} height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
                <div className="col">
                   <ul className ="list-unstyled">
-                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img src={linkedIn}height ="50px" width="50px"/></a></li>
+                      <li className="footer"><a href="https://www.linkedin.com/in/donnie-rawlings-7b33941a7" target="_blank" rel="noopener noreferrer"><img className="spin-on-hover" src={linkedIn}height ="50px" width="50px"/></a></li>
                   </ul>
                </div>
              </div>
