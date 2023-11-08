4a9840a3d433d71dfe5be5331cc4cf8ff2d54f3e
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-08 15:27:02 -0600

    increase text size on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -21,19 +21,19 @@ export default function AboutPage() {
           <div className="col-md-6">
 
     <p className="lead text-zoom">
-            My name is Donnie Rawlings, and I decided to change my career after twenty-two years of working in retail and restaurant upper management. 
-        I enjoy problem solving and finding unique ways to overcome daily hurdles as they arise needing quick solutions. 
+           <strong> <font size="+2">My name is Donnie Rawlings, and I decided to change my career after twenty-two years of working in retail and restaurant upper management. 
+        I enjoy problem solving and finding unique ways to overcome daily hurdles as they arise needing quick solutions.</font> </strong>
     </p>
     
     <p className="lead text-zoom">    
-        In the last five years I have been involved in the roll out of multiple new divisions of my company. Most recently as part of enhancing the performace of my division I have been involved
+        <strong><font size="+2">In the last five years I have been involved in the roll out of multiple new divisions of my company. Most recently as part of enhancing the performace of my division I have been involved
         with teams to develop applications and websites to improve efficiency. I have really thrived in that space. This made me excited to make a leap into a new field. I am certain that joining your
-        team would be mutually benificial. 
+        team would be mutually benificial.</font> </strong>
     </p>
     
     <p className="lead text-zoom">
-        I bring with me not only decades of problem solving, but also the experience of managing teams through constantly chaning work environments. I have my MBA in business managment,
-        and am currently working through a full stack bootcamp at UC Davis.
+        <strong><font size="+2">I bring with me not only decades of problem solving, but also the experience of managing teams through constantly chaning work environments. I have my MBA in business managment,
+        and am currently working through a full stack bootcamp at UC Davis.</font></strong>
     
     </p>
         </div>
